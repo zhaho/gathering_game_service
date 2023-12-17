bf08e469ecd52fb1535208fa82eb99e799ec8a40
zhaho <zhaho.ros@gmail.com>
2023-12-17 21:23:08 +0100

    tests added

---
+++ game/test/test_game_info.py
@@ -0,0 +1,29 @@
+from main import game_info
+
+#game = game_info("63706")
+game = game_info("116954")
+
+print("\nValidating game: " + game.title())
+print("\nCategories: "+game.category())
+print("\nMechanics: "+game.mechanic())
+
+def test_valid():
+    assert game.is_valid() == True
+        
+def test_titles():
+    assert len(game.title()) > 0
+    assert type(game.title()) == str
+
+def test_categories():
+    assert len(game.category()) > 0
+    assert type(game.category()) == str
+
+def test_mechanics():
+    assert len(game.mechanic()) > 0
+    assert type(game.mechanic()) == str
+
+def test_bgg_rating():
+    assert len(str(game.bgg_rating())) > 0
+    assert type(game.bgg_rating()) == int
+
+
